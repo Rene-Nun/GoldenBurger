6c55e80989240a2d616e25cee9c718bfca96715a
Rene-Nun <christiannuntkd@hotmail.com>
2026-08-11 14:19:09 -0600

    Actualizar BurgerScene.jsx

--- components/BurgerScene.jsx
+++ components/BurgerScene.jsx
@@ -57,7 +57,7 @@ export default function BurgerScene({ explode = 0 }) {
         </group>
 
         {/* Hamburguesa de enfrente — la que explota */}
-        <group rotation={[-0.15, 0.3, 0]}>
+        <group rotation={[-0.15, -0.3, 0]}>
           <BurgerModel
             explode={explode}
             axisX={0.6}
